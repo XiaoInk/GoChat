c1d9e0f999fcf66ec4a82512fdf5480b9077816c
XiaoInk <xiaoqiangink@outlook.com>
2021-03-25 16:52:38 +0800

    客户端实现私聊模式

--- src/client.go
+++ src/client.go
@@ -67,7 +67,7 @@ func (c *Client) Menu() bool {
 	case 1: // 群聊模式
 		c.PublicChat()
 	case 2: // 私聊模式
-		fmt.Println("私聊模式")
+		c.PrivateChat()
 	case 3: // 修改用户名
 		fmt.Println("修改用户名")
 	case 0: // 退出
@@ -85,6 +85,28 @@ func (c *Client) PublicChat() {
 		}
 
 		c.SendMsg(c.chatMsg) // 向服务器发送消息
+	}
+}
+
+func (c *Client) PrivateChat() {
+	var toUser string
+
+	c.SendMsg("?who") // 获取在线用户列表
+
+	fmt.Println(">>> 您已进入私聊模式，请输入聊天对象[用户名]，输入 ?exit 退出私聊模式")
+	_, _ = fmt.Scanln(&toUser)
+	if toUser == "?exit" {
+		return
+	}
+
+	fmt.Println(">>> 请输入聊天内容")
+	for {
+		_, _ = fmt.Scanln(&c.chatMsg)
+		if c.chatMsg == "?exit" {
+			break
+		}
+
+		c.SendMsg("?to:" + toUser + ":" + c.chatMsg) // 向给定用户发送消息
 	}
 }
 
